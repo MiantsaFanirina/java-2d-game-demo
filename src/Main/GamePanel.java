19297391e550a556ddbf45d847c3d9f8f752e560
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2026-02-21 00:40:09 +0400

    add Thread

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,7 +3,7 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     // SCREEM SETTINGS
     final int originalTileSize = 16;
     final int scale = 3;
@@ -13,9 +13,49 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+
+            // UPDATE
+            update();
+
+            // DRAW
+            repaint();
+
+
+        }
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        // DRAW
+        g2.setColor(Color.white);
+        g2.fillRect(tileSize, tileSize, tileSize, tileSize);
+        g2.dispose();
+    }
+
+
 }
